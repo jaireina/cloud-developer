d35c50398d9b0b2fce5d850cd5f2686dfbec689b
Jair Reina <jair.reina@digitas.com>
2020-12-27 14:40:16 -0600

    adding concat function and unit tests

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,40 +1,51 @@
-import { add, divide } from './units';
+import { add, divide, concat } from "./units";
 
-import { expect } from 'chai';
-import 'mocha';
+import { expect } from "chai";
+import "mocha";
+import { isRegExp } from "util";
 
-describe('add function', () => {
-
-  it('should add two and two', () => {
-    const result = add(2,2);
+describe("add function", () => {
+  it("should add two and two", () => {
+    const result = add(2, 2);
     expect(result).to.equal(4);
   });
 
-  it('should add -2 and two', () => {
-    const result = add(-2,2);
+  it("should add -2 and two", () => {
+    const result = add(-2, 2);
     expect(result).to.equal(0);
   });
-
 });
 
-describe('divide', () => {
-
-  it('should divide 6 by 3', () => {
-    const result = divide(6,3);
+describe("divide", () => {
+  it("should divide 6 by 3", () => {
+    const result = divide(6, 3);
     expect(result).to.equal(2);
   });
 
-  it('should divide 5 and 2', () => {
-    const result = divide(5,2);
+  it("should divide 5 and 2", () => {
+    const result = divide(5, 2);
     expect(result).to.equal(2.5);
   });
 
-  it('should throw an error if div by zero', () => {
-    expect(()=>{ divide(5,0) }).to.throw('div by 0')
+  it("should throw an error if div by zero", () => {
+    expect(() => {
+      divide(5, 0);
+    }).to.throw("div by 0");
   });
-
 });
 
-// @TODO try creating a new describe block for the "concat" method
-// it should contain an it block for each it statement in the units.ts @TODO.
-// don't forget to import the method ;)
+describe("concat function", () => {
+  it("it should take two string paramaters", () => {
+    expect(concat("a", "b")).to.equal("ab");
+  });
+
+  it("should return one string combining the two strings", () => {
+    expect(concat("concat ", "this")).to.equal("concat this");
+  });
+
+  it("should throw an error if either of the strings are empty.", () => {
+    expect(() => {
+      concat("", "");
+    }).to.throw("empty string provided");
+  });
+});
